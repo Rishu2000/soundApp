b5b43a1ec0858848adf6147caeafdcf54b5c1427
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-17 23:55:52 +0530

    Completed the spanish number voice play App.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react"
-import {Text,View,ScrollView,Image} from "react-native"
+import {Text,View,ScrollView,Image,TouchableOpacity,StyleSheet} from "react-native"
+import Sound from "react-native-sound"
 const soundList = [
   require("./assets/one.wav"),
   require("./assets/two.wav"),
@@ -14,11 +15,62 @@ const soundList = [
 ]
 
 const App = () => {
+
+  const playSound = (sound) => {
+    const soundVoice = new Sound(sound,Sound.MAIN_BUNDLE, (err) => {
+      if(err){
+        console.log("Not able play sound.")
+      }
+    })
+    setTimeout(() => {
+      soundVoice.play()
+    },500)
+    soundVoice.release()
+  }
+
   return(
-    <ScrollView>
-      <Image source={require("./assets/logo.png")}/>
+    <ScrollView style={styles.container}>
+      <Image source={require("./assets/logo.png")} style={styles.img}/>
+      <View style={styles.gridContainer}>
+          {soundList.map((sound) => (
+        <TouchableOpacity key={sound} style={styles.box} 
+            onPress={() => playSound(sound)}>
+          <Text style={styles.text}>{sound}</Text>
+          </TouchableOpacity>
+          ))}
+      </View>
     </ScrollView>
   )
 }
 
 export default App
+
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#ff0"
+  },
+  img:{
+    alignSelf:"center",
+    marginTop:20
+  },
+  gridContainer:{
+    flex:1,
+    flexDirection:"row",
+    flexWrap:"wrap",
+    alignItems:"flex-start",
+    justifyContent:"space-around"
+  },
+  box:{
+    padding:40,
+    backgroundColor:"#00f",
+    marginBottom:10,
+    borderRadius:5
+  },
+  text:{
+    fontSize:50,
+    color:"#fff"
+  }
+})
