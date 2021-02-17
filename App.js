fc63eb1366d82f91413c2244b37ad609c930ba2b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-17 16:09:38 +0530

    Old way of importing and also multiple importing using Array.

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 import React from "react"
 import {Text,View,ScrollView,Image} from "react-native"
+const soundList = [
+  require("./assets/one.wav"),
+  require("./assets/two.wav"),
+  require("./assets/three.wav"),
+  require("./assets/four.wav"),
+  require("./assets/five.wav"),
+  require("./assets/six.wav"),
+  require("./assets/seven.wav"),
+  require("./assets/eight.wav"),
+  require("./assets/nine.wav"),
+  require("./assets/ten.wav")
+]
 
 const App = () => {
   return(
